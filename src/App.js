f02cfbfeba9380df03148176072056712e9f2b74
Abdul Rafi <arafi6624@bths.edu>
2022-12-21 23:44:21 -0500

    Naive algorithm complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Naive from "./components/Naive";
 
 function App() {
+
+  const [text, setText] = useState('');
+  const [pattern, setPattern] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <h1 className="container">String Matcher</h1>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="text" className="form-label">Text</label>
+            <textarea className="form-control" id="text" value={text} onChange={(e) => {setText(e.target.value); setSubmitted(false);}}/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="pattern" className="form-label">Pattern</label>
+            <input type="text" className="form-control" id="pattern" value={pattern} onChange={(e) => {setPattern(e.target.value); setSubmitted(false);}}/>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>  
+        {submitted && <Naive text={text} pattern={pattern} />}
+      </div> 
     </div>
   );
 }
